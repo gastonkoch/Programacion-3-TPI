523decad80e88b9cc23ba7b8a6ef26b1bf398350
GASTON KOCH <GASTON.KOCH@hotmail.com>
2024-05-18 12:17:49 -0300

    Customer camino sin ORM 2

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6bb1d7004d39157972c9dd355aad1630f303b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5170b75f11744fc89764a1ca5e91d1ee4afd4c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
